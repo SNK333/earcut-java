f151017b44c82df1c7ed5880e86807bd4595879c
SNK <54633342+SNK@users.noreply.github.com>
2020-10-03 17:42:49 +0530

    Update EarCut.java

--- EarCut.java
+++ EarCut.java
@@ -8,8 +8,8 @@ import java.util.List;
 /**
  * Copyright 2020 the3deers.org
  *
- *  is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * P is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ *  deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
